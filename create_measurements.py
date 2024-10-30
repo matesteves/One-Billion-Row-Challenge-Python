346d36c844002609e85bb99da203713aa799b8df
Matheus Esteves <mat.esteves@gmail.com>
2024-10-30 01:04:11 -0300

    Change file encoding

--- create_measurements.py
+++ create_measurements.py
@@ -42,7 +42,7 @@ def build_weather_station_name_list():
     Grabs the weather station names from example data provided in repo and dedups
     """
     station_names = []
-    with open('../../../data/weather_stations.csv', 'r') as file:
+    with open('../../../data/weather_stations.csv', 'r', encoding='utf-8') as file:
         file_contents = file.read()
     for station in file_contents.splitlines():
         if "#" in station:
@@ -111,7 +111,7 @@ def build_test_data(weather_station_names, num_rows_to_create):
     print('Building test data...')
 
     try:
-        with open("../../../data/measurements.txt", 'w') as file:
+        with open("../../../data/measurements.txt", 'w', encoding='utf-8') as file:
             progress = 0
             for chunk in range(chunks):
                 
